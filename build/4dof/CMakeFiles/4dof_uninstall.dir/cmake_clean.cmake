cb1aa8c1fd46c13dbe1daba0dc4c73c4fee30abe
Welt-liu <1994524450@qq.com>
2024-10-15 14:12:12 +0800

    4dof模型完成

---
+++ build/4dof/CMakeFiles/4dof_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/4dof_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/4dof_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
