ea09f6a8574324903e82c5f0f325f6efd8493c76
Welt-liu <1994524450@qq.com>
2024-10-19 21:54:30 +0800

    save

--- _whoami.sh
+++ _whoami.sh
@@ -7,3 +7,8 @@ ls /dev/ttyUSB*
 # ros2 run fsrobo_a1_serve_py follower
 # 1018 新测试
 # //new_moveit_test test作废
+
+
+#4dof4_robot :小球在的轴三倒数第二个 new_moveit_test
+#roarm_ws_em0：微雪的
+#test4_dof_robot：新弄得urdf
