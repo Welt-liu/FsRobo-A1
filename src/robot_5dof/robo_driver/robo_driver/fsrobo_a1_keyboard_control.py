10197bdc2b1162c20b22113cf2eaa4bf382ac100
Welt-liu <1994524450@qq.com>
2024-11-01 11:43:40 +0800

    save

--- src/robot_5dof/robo_driver/robo_driver/fsrobo_a1_keyboard_control.py
+++ src/robot_5dof/robo_driver/robo_driver/fsrobo_a1_keyboard_control.py
@@ -11,10 +11,13 @@ from rclpy.node import Node
 import curses
 # from rclpy.node import Node
 from sensor_msgs.msg import JointState
+from robo_interfaces.srv import RoboStates
+
+JOINT_NAME = ['robot_joint1', 'robot_joint2', 'robot_joint3', 'robot_joint4', 'hand_joint', 'left_joint']
+
 
 class angle_publisher_(Node):
-    
-    # keyboard = keyboard_control()
+    #初始化节点
     def __init__(self):
         super().__init__('keyboard_control_node')
         self.angle_publishers = self.create_publisher(
@@ -25,25 +28,43 @@ class angle_publisher_(Node):
     def publish_angle(self,_position):
         joint_state = JointState()
         # joint_state.header.stamp = self.get_clock().now().to_msg()
-        joint_state.name = ['robot_joint1', 'robot_joint2', 'robot_joint3', 'robot_joint4', 'hand_joint', 'left_joint']
+        joint_state.name = JOINT_NAME
         
         position = list(map(float, _position))  # 转换为浮点数数组
         joint_state.position = position
         self.angle_publishers.publish(joint_state)
 
+class servo_servicer_(Node):
+
+    def __init__(self):
+        super().__init__('servo_servicer_node')
+        self.cli = self.create_client(RoboStates, 'robo_states')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = RoboStates.Request()
+
+    def send_servo_request(self, command):
+        self.req.command = command
+        self.future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+
+
+
 
 class keyboard_control_:
     
-
-    MIN_ANGLES = [-2.0, -0.9, -2.1, -2, -0.5, -1.1]  # 示例最小值
-    MAX_ANGLES = [2.0, 1.5, 2.1, 1.5, 3.1, 0.0]         # 示例最大值    
+    MIN_ANGLES = [-2.0, -0.9, -2.1, -2, -0.5, -1.1]  # 角度最小值
+    MAX_ANGLES = [2.0, 1.5, 2.1, 1.5, 3.1, 0.0]         # 角度最大值    
     INCREMENT_VALUE = 0.087 # 按下一次按键时，增加的角度
 
-    angles = [0.0,0.0,0.0,0.0,0.0,0.0] 
+    set_angle = [0.0,0.0,0.0,0.0,0.0,0.0] 
     cur_control_joint = 1  # 当前控制的关节编号
+    cur_angle =[0.0,0.0,0.0,0.0,0.0,0.0]
 
     def __init__(self):
         self.angle_publisher = angle_publisher_()
+        self.servo_servicer = servo_servicer_()
         curses.wrapper(self.loop_) #启动wrapper，传入loop_函数
 
     def refresh_(self,stdscr):
@@ -51,9 +72,13 @@ class keyboard_control_:
         stdscr.clear()
         stdscr.addstr(0,0,'请确认终端显示行/列数是否足够\n')
         stdscr.addstr(1,0,'键盘1~6切换控制关节(包括末端夹爪),键盘上下键增减关节角度,S键发送命令,按下q退出\n')
-        angles_str = ', '.join(f'关节{i}: {self.angles[i-1]:.2f}' for i in range(1, 7))  # 格式化角度
-        stdscr.addstr(3,0,f'正在修改关节{self.cur_control_joint}') 
-        stdscr.addstr(4,0,f'当前角度: {angles_str}') 
+        stdscr.addstr(3,0,f'正在修改关节{self.cur_control_joint}')
+
+        angles_str = ', '.join(f'关节{i}: {self.set_angle[i-1]:.2f}' for i in range(1, 7))  # 格式化角度
+        stdscr.addstr(4,0,f'当前设置角度: {angles_str}')
+
+        cur_angle_str = ', '.join(f'关节{i}: {self.cur_angle[i-1]:.2f}' for i in range(1, 7))  # 格式化角度
+        stdscr.addstr(5,0,f'回读角度: {cur_angle_str}')
         stdscr.refresh()
         
     def loop_(self,stdscr):
@@ -68,22 +93,25 @@ class keyboard_control_:
 
             elif key == ord('1'):
                 self.cur_control_joint = 1
-                self.angle_publisher.publish_angle(self.angles)
+                self.angle_publisher.publish_angle(self.set_angle)
             elif key == ord('2'):
-                self.cur_control_joint = 2
-            elif key == ord('3'):
-                self.cur_control_joint = 3
-            elif key == ord('4'):
-                self.cur_control_joint = 4
-            elif key == ord('5'):
-                self.cur_control_joint = 5
-            elif key == ord('6'):
-                self.cur_control_joint = 6
+                response  = self.servo_servicer.send_servo_request('angle')
+                for i in range(len(response.servo_name)):
+                    index = JOINT_NAME.index(response.servo_name[i])
+                    self.cur_angle[index] = response.servo_data[i]
+            # elif key == ord('3'):
+            #     self.cur_control_joint = 3
+            # elif key == ord('4'):
+            #     self.cur_control_joint = 4
+            # elif key == ord('5'):
+            #     self.cur_control_joint = 5
+            # elif key == ord('6'):
+            #     self.cur_control_joint = 6
             
             elif key == curses.KEY_UP:
-                self.angles[self.cur_control_joint-1] += self.INCREMENT_VALUE
+                self.set_angle[self.cur_control_joint-1] += self.INCREMENT_VALUE
             elif key == curses.KEY_DOWN:
-                self.angles[self.cur_control_joint-1] -= self.INCREMENT_VALUE
+                self.set_angle[self.cur_control_joint-1] -= self.INCREMENT_VALUE
             elif key == curses.KEY_LEFT:
                 if  self.cur_control_joint  > 1:
                     self.cur_control_joint -=1
@@ -91,30 +119,22 @@ class keyboard_control_:
                 if  self.cur_control_joint  < 6:
                     self.cur_control_joint +=1
             elif key == ord('s'):  # 按下S键发送命令
-                self.angle_publisher.publish_angle(self.angles)
+                self.angle_publisher.publish_angle(self.set_angle)
                 
-            for i in range(len(self.angles)):
-                if self.angles[i] < self.MIN_ANGLES[i]:
-                    self.angles[i] = self.MIN_ANGLES[i]
-                elif self.angles[i] > self.MAX_ANGLES[i]:
-                    self.angles[i] = self.MAX_ANGLES[i]
+            for i in range(len(self.set_angle)):
+                if self.set_angle[i] < self.MIN_ANGLES[i]:
+                    self.set_angle[i] = self.MIN_ANGLES[i]
+                elif self.set_angle[i] > self.MAX_ANGLES[i]:
+                    self.set_angle[i] = self.MAX_ANGLES[i]
             
             self.refresh_(stdscr)
 
-
-
-        
-
 def main(args=None):
-    
+
     rclpy.init(args=args)
-    
     keyboard_contro = keyboard_control_()
-    
     rclpy.spin(keyboard_contro.angle_publisher)
-
     keyboard_contro.angle_publisher.destroy_node()
-
     rclpy.shutdown()
     
 if __name__ == '__main__':
--- src/robot_5dof/robo_driver/robo_driver/robo_arm_control_node.py
+++ src/robot_5dof/robo_driver/robo_driver/robo_arm_control_node.py
@@ -16,19 +16,20 @@ from sensor_msgs.msg import JointState
 import math
 import time
 # from std_msgs.msg import Float32MultiArray
-
 from robo_interfaces.srv import RoboStates
+
 def radians_to_degrees(radians):
     degrees = radians * (180 / math.pi)
     return degrees
-def get_keys_by_value(d, target_value):
-    return [key for key, value in d.items() if value == target_value]
+
 
 class Arm_contorl(Node):
 
-    SERVO_PORT_NAME =  u'/dev/ttyUSB4'      # 舵机串口号 <<< 修改为实际串口号
+    SERVO_PORT_NAME =  u'/dev/ttyUSB1'      # 舵机串口号 <<< 修改为实际串口号
     SERVO_BAUDRATE = 115200                 # 舵机的波特率
-    joint_ = {'robot_joint1':0,'robot_joint2':1,'robot_joint3':2,'robot_joint4':3,'hand_joint':4,'left_joint':5,'right_joint':99}
+    joint_ = ['robot_joint1','robot_joint2','robot_joint3','robot_joint4','hand_joint','left_joint','right_joint']
+    index_joint_ = {value: index for index, value in enumerate(joint_)}
+
     last_angle = {0,0,0,0,0,0}
     def __init__(self):
         super().__init__('robo_arm_control_node')
@@ -58,41 +59,40 @@ class Arm_contorl(Node):
 
     # 判断关节名称和位置，转换为舵机角度
     def convert_to_servo_angle(self,joint_name,joint_postion):
-        if self.joint_[joint_name] == 0:
+        if joint_name == self.joint_[0]:
             self.uservo.set_servo_angle(0,-radians_to_degrees(joint_postion),interval=1000)
-        elif self.joint_[joint_name] == 1:
+        elif joint_name == self.joint_[1]:
             self.uservo.set_servo_angle(1,radians_to_degrees(joint_postion),interval=1000)
-        elif self.joint_[joint_name] == 2:
+        elif joint_name == self.joint_[2]:
             self.uservo.set_servo_angle(2,-radians_to_degrees(joint_postion),interval=1000)
-        elif self.joint_[joint_name] == 3:
+        elif joint_name == self.joint_[3]:
             self.uservo.set_servo_angle(3,radians_to_degrees(joint_postion),interval=1000)
-        elif self.joint_[joint_name] == 4:
+        elif joint_name == self.joint_[4]:
             self.uservo.set_servo_angle(4,-90+radians_to_degrees(joint_postion),interval=1000)
-        elif self.joint_[joint_name] == 5:
+        elif joint_name == self.joint_[5]:
             self.uservo.set_servo_angle(5,-radians_to_degrees(joint_postion),interval=1000)
 
     # 话题接收消息处理
     def set_servo_angle_callback(self,msg):
         for i in range(len(msg.name)):  
+            if self.index_joint_[msg.name[i]] not in self.uservo.servos:
+                print(f"舵机{msg.name[i]}未连接")
+                continue
             self.convert_to_servo_angle(joint_postion = msg.position[i],joint_name = msg.name[i])
         self.uservo.wait( timeout=1.01)
-    
+
+    # 反馈舵机状态消息处理
     def query_data_callback(self, request, response):
-        request_type = request.query_type
-        match request_type:
+        command = request.command
+        match command:
             case 'angle':
                 self.uservo.query_all_srv_angle()
                 for i in self.uservo.servos:
-                    response.servo_names.append( get_keys_by_value(self.joint_,i))
-                    response.servo_states.append(self.uservo.servos[i].angle)
+                    response.servo_name.append(self.joint_[i])
+                    response.servo_data.append(self.uservo.servos[i].angle)
                 return response
 
         
-        
-
-
-
-
 def main(args=None):
         rclpy.init(args=args)
         followerarm_subscriber = Arm_contorl()
